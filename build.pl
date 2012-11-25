996234c8f54c2db1119b37f0c7b6bb2198fb9b34
Gabor Szabo <szabgab@gmail.com>
2012-11-25 08:50:30 +0200

    build script now works in Git repository

--- build.pl
+++ build.pl
@@ -2,21 +2,26 @@
 use strict;
 use warnings;
 
-use File::Temp qw(tempdir);
+#use File::Temp qw(tempdir);
 use Cwd qw(cwd);
-
-my $dir = tempdir(CLEANUP => 1);
-
+#
+#my $dir = tempdir(CLEANUP => 1);
+#
 my $home = cwd;
 END {
     chdir $home;
 }
 
-chdir $dir;
-system "svn export https://svn1.hostlocal.com/gabor/training/html5/examples/regex-maven";
+chdir '..';
+#chdir $dir;
+#system "svn export https://svn1.hostlocal.com/gabor/training/html5/examples/regex-maven";
+
+
+my $cmd = "zip -r regex-maven.zip regex-maven -x \\*/.git/\\* 0x \\*.swp";
 foreach my $f (qw(app.psgi build.pl README.txt)) {
-    unlink "regex-maven/$f";
+    $cmd .= " -x regex-maven/$f";
 }
 
-system "zip -r $home/regex-maven.zip regex-maven";
+system $cmd;
+
 
