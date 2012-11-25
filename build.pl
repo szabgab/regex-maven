c55df4826db7607ba80082568ed4d4cfac1051c2
Gabor Szabo <szabgab@gmail.com>
2012-11-25 08:51:06 +0200

    remove commented code

--- build.pl
+++ build.pl
@@ -2,19 +2,13 @@
 use strict;
 use warnings;
 
-#use File::Temp qw(tempdir);
 use Cwd qw(cwd);
-#
-#my $dir = tempdir(CLEANUP => 1);
-#
 my $home = cwd;
 END {
     chdir $home;
 }
 
 chdir '..';
-#chdir $dir;
-#system "svn export https://svn1.hostlocal.com/gabor/training/html5/examples/regex-maven";
 
 
 my $cmd = "zip -r regex-maven.zip regex-maven -x \\*/.git/\\* 0x \\*.swp";
