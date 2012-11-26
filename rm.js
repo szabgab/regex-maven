962b161d8d3adbebf677e1ce7e4ce43014c3effb
Gabor Szabo <szabgab@gmail.com>
2012-11-26 13:57:26 +0200

    improve the quiz

--- rm.js
+++ rm.js
@@ -1,6 +1,3 @@
-function hello() {
-}
-
 //var btn_learn = document.querySelector('button#btn-learn');
 //function goto_learn() {
 //    h1.classList.toggle('large');
@@ -23,19 +20,36 @@ function hello() {
 //    cheat_all.addEventListener('click', show_cheat_all );
 //}
 
-$('div.ui-page').live("swipeleft", function(){
-    var nextpage = $(this).next('div[data-role="page"]');
-    // swipe using id of next page if exists
-    if (nextpage.length > 0) {
-        $.mobile.changePage(nextpage, 'slide');
-    }
-});
+// $('div.ui-page').live("swipeleft", function(){
+//     var nextpage = $(this).next('div[data-role="page"]');
+//     // swipe using id of next page if exists
+//     if (nextpage.length > 0) {
+//         $.mobile.changePage(nextpage, 'slide');
+//     }
+// });
+//
+// $('div.ui-page').live("swiperight", function(){
+//     var prevpage = $(this).prev('div[data-role="page"]');
+//     // swipe using id of next page if exists
+//     if (prevpage.length > 0) {
+//         $.mobile.changePage(prevpage, 'slide', true);
+//     }
+// });
 
-$('div.ui-page').live("swiperight", function(){
-    var prevpage = $(this).prev('div[data-role="page"]');
-    // swipe using id of next page if exists
-    if (prevpage.length > 0) {
-        $.mobile.changePage(prevpage, 'slide', true);
+function check_quiz() {
+    var c = document.getElementById('count');
+    for (var i = 0; i < c.value; i++) {
+        var user_id = 'checkbox-mini-' + i;
+        var expected_id = 'expected-' + i;
+        var user = document.getElementById(user_id).checked ? 'true' : 'false';
+        var expected = document.getElementById(expected_id).value;
+        if (user !== expected) {
+            //alert(i + ' ' + user + ' ' + expected);
+            $( "#failure" ).popup( "open" )
+            break;
+        }
     }
-});
+    $( "#success" ).popup( "open" )
+}
+
 
