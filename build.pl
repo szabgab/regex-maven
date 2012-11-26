eaffefb11bdb18d7a2f17fc846f9615bd8e052e0
Gabor Szabo <szabgab@gmail.com>
2012-11-26 22:38:07 +0200

    change build script to match the new layout and to check version numbers

--- build.pl
+++ build.pl
@@ -3,19 +3,52 @@ use strict;
 use warnings;
 
 use Cwd qw(cwd);
-my $home = cwd;
-END {
-    chdir $home;
-}
+#my $home = cwd;
+#END {
+#    chdir $home;
+#}
+#
+#chdir '..';
 
-chdir '..';
+# Check for version number
 
 
-my $cmd = "zip -r regex-maven.zip regex-maven -x \\*/.git/\\* 0x \\*.swp";
+my ($versionCode, $version, $about) = get_versions();
+die if not $version;
+die if $version ne $versionCode or $version ne $about;
+
+my $cmd = "zip -r regex-maven-$version.zip app -x \\*/.git/\\* 0x \\*.swp";
 foreach my $f (qw(app.psgi build.pl README.txt .gitignore)) {
     $cmd .= " -x regex-maven/$f";
 }
 
 system $cmd;
+
+
+sub get_versions {
+    my $versionCode;
+    my $version;
+    my $about;
+
+    open my $fh, '<', 'app/config.xml' or die;
+    while (my $line = <$fh>) {
+        chomp $line;
+        if ($line =~ /versionCode\s*=\s*"(\d+)"/) {
+            $versionCode = $1;
+        }
+        if ($line =~ /version\s*=\s*"0\.0\.(\d+)"/) {
+            $version = $1;
+        }
+    }
+    open my $in, '<', 'app/index.html' or die;
+    while (my $line = <$in>) {
+        chomp $line;
+        if ($line =~/version\s+0\.0\.(\d+)/) {
+            $about = $1;
+        }
+    }
+    return $versionCode, $version, $about;
+}
+
 
 
